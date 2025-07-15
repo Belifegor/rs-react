2482d4a5359b9733c7e6ce2a6e17c1de3ae34157
github-usernamebelifegor <hydrox27@gmail.com>
2025-07-15 23:55:32 +0300

    feat: add ErrorBoundry

---
+++ src/components/ErrorBoundary.tsx
@@ -0,0 +1,35 @@
+import { Component } from 'react';
+import type { ReactNode } from 'react';
+
+type Props = {
+  children: ReactNode;
+  fallback: ReactNode;
+};
+
+type State = {
+  hasError: boolean;
+};
+
+class ErrorBoundary extends Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(): State {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error: Error, errorInfo: React.ErrorInfo): void {
+    console.error('Uncaught error:', error, errorInfo);
+  }
+
+  render(): ReactNode {
+    if (this.state.hasError) {
+      return this.props.fallback;
+    }
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,15 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
+import ErrorBoundary from './components/ErrorBoundary.tsx';
 
 const rootElement = document.getElementById('root');
 if (rootElement) {
   createRoot(rootElement).render(
     <StrictMode>
-      <App />
+      <ErrorBoundary fallback={<p>Something went wrong</p>}>
+        <App />
+      </ErrorBoundary>
     </StrictMode>
   );
 }
