65e122f7362b879b609818ed9b4d2764277213d2
github-usernamebelifegor <hydrox27@gmail.com>
2025-07-25 21:40:54 +0300

    ref(App): change class to func component

--- src/App.tsx
+++ src/App.tsx
@@ -1,132 +1,93 @@
 import '../src/assets/styles/App.css';
-import { Component } from 'react';
+import { useEffect, useState } from 'react';
 import Search from './components/Search';
-import type { AppState } from './types/types';
+import type { Character } from './types/types';
 import Card from './components/Card';
 import { fetchCharacters, fetchPageData } from './api/rickAndMorty';
 
-class App extends Component<object, AppState> {
-  constructor(props: object) {
-    super(props);
-    this.state = {
-      searchTerm: '',
-      loading: false,
-      results: [],
-      error: null,
-      nextPage: null,
-      prevPage: null,
-      hasError: false,
-    };
-  }
+export default function App() {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [results, setResults] = useState<Character[]>([]);
+  const [error, setError] = useState<string | null>(null);
+  const [nextPage, setNextPage] = useState<string | null>(null);
+  const [prevPage, setPrevPage] = useState<string | null>(null);
 
-  componentDidMount(): void {
+  useEffect(() => {
     const savedTerm = localStorage.getItem('search') || '';
-    this.setState({ searchTerm: savedTerm }, () => {
-      this.fetchData(savedTerm);
-    });
-  }
+    setSearchTerm(savedTerm);
+    fetchData(savedTerm);
+  }, []);
 
-  fetchData = (term: string) => {
+  const fetchData = (term: string) => {
     const query = term.trim().toLowerCase();
     localStorage.setItem('search', query);
 
-    this.setState({ loading: true, error: null });
+    setLoading(true);
+    setError(null);
 
     fetchCharacters(query)
       .then((data) => {
-        this.setState({
-          results: data.results,
-          searchTerm: term,
-          nextPage: data.info.next,
-          prevPage: data.info.prev,
-          loading: false,
-        });
+        setResults(data.results);
+        setSearchTerm(term);
+        setNextPage(data.info.next);
+        setPrevPage(data.info.prev);
+        setLoading(false);
       })
       .catch((error) => {
-        this.setState({ error: error.message, loading: false });
+        setError(error.message);
+        setLoading(false);
       });
   };
 
-  fetchPage = (url: string) => {
-    this.setState({ loading: true, error: null });
+  const fetchPage = (url: string) => {
+    setLoading(true);
+    setError(null);
 
     fetchPageData(url)
       .then((data) => {
-        this.setState({
-          results: data.results,
-          nextPage: data.info.next,
-          prevPage: data.info.prev,
-          loading: false,
-        });
+        setResults(data.results);
+        setNextPage(data.info.next);
+        setPrevPage(data.info.prev);
+        setLoading(false);
       })
       .catch((error) => {
-        this.setState({ error: error.message, loading: false });
+        setError(error.message);
+        setLoading(false);
       });
   };
-  throwError = () => {
-    this.setState({ hasError: true });
-  };
 
-  render() {
-    const { searchTerm, results, loading, error } = this.state;
+  return (
+    <>
+      <Search onSearch={fetchData} initialValue={searchTerm} />
+      <div>
+        <h1 className="mb-4">Rick and Morty Characters</h1>
+        <p className="mb-4 text-2xl text-stone-300">
+          Search Term: <strong>{searchTerm}</strong>
+        </p>
 
-    if (this.state.hasError) {
-      throw new Error('Simulate error');
-    }
+        {loading && <p>Loading...</p>}
+        {error && <p style={{ color: 'red' }}>Error: {error}</p>}
 
-    return (
-      <>
-        <Search
-          onSearch={this.fetchData}
-          initialValue={this.state.searchTerm}
-        />
-        <div>
-          <h1 className="mb-4">Rick and Morty Characters</h1>
-          <p className="mb-4 text-2xl text-stone-300">
-            Search Term: <strong>{searchTerm}</strong>
-          </p>
-          {loading && <p>Loading...</p>}
-          {error && <p style={{ color: 'red' }}>Error: {error}</p>}
-          <div className="grid grid-cols-1 sm:grid-cols-4 sm:gap-6">
-            {results.map((char) => (
-              <Card key={char.id} character={char} />
-            ))}
-          </div>
-          <button
-            onClick={this.throwError}
-            className="mt-5 px-4 py-2 bg-red-600 text-white rounded-md hover:bg-red-700 transition-colors"
-          >
-            Simulate Error
-          </button>
-          <div style={{ marginTop: '1rem' }}>
-            {this.state.prevPage && (
-              <button
-                onClick={() => {
-                  if (this.state.prevPage) {
-                    this.fetchPage(this.state.prevPage);
-                  }
-                }}
-              >
-                Previous
-              </button>
-            )}
-            {this.state.nextPage && (
-              <button
-                onClick={() => {
-                  if (this.state.nextPage) {
-                    this.fetchPage(this.state.nextPage);
-                  }
-                }}
-                style={{ marginLeft: '10px' }}
-              >
-                Next
-              </button>
-            )}
-          </div>
+        <div className="grid grid-cols-1 sm:grid-cols-4 sm:gap-6">
+          {results.map((char) => (
+            <Card key={char.id} character={char} />
+          ))}
         </div>
-      </>
-    );
-  }
+        <div style={{ marginTop: '1rem' }}>
+          {prevPage && (
+            <button onClick={() => fetchPage(prevPage)}>Previous</button>
+          )}
+          {nextPage && (
+            <button
+              onClick={() => fetchPage(nextPage)}
+              style={{ marginLeft: '10px' }}
+            >
+              Next
+            </button>
+          )}
+        </div>
+      </div>
+    </>
+  );
 }
-
-export default App;
