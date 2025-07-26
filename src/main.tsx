d2c6cc99550e7179a9a64703ea4a8a9f2c5a0e5e
github-usernamebelifegor <hydrox27@gmail.com>
2025-07-26 19:48:57 +0300

    ref: add routes to main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router';
+import { BrowserRouter, Routes, Route } from 'react-router';
 import '../src/assets/styles/index.css';
 import App from './App.tsx';
 import ErrorBoundary from './components/ErrorBoundary.tsx';
@@ -11,7 +11,11 @@ if (rootElement) {
     <StrictMode>
       <BrowserRouter>
         <ErrorBoundary>
-          <App />
+          <Routes>
+            <Route path="/" element={<App />} />
+            <Route path="/:page" element={<App />} />
+            <Route path="/:page/:detailsId" element={<App />} />
+          </Routes>
         </ErrorBoundary>
       </BrowserRouter>
     </StrictMode>
