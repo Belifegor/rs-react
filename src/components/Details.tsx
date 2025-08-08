2dd9e25a3732bb54de819d10df141da673a07bb4
github-usernamebelifegor <hydrox27@gmail.com>
2025-08-08 23:25:03 +0300

    ref: change fetch to query of tanstack

--- src/components/Details.tsx
+++ src/components/Details.tsx
@@ -1,38 +1,21 @@
-import { useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
-import type { Character } from '../types/types';
+import { useCharacterDetailsQuery } from '../hooks/useCharacterDetailsQuery';
 
 export default function Details() {
   const navigate = useNavigate();
   const { detailsId, page = '1' } = useParams();
+  const {
+    data: character,
+    isLoading,
+    error,
+  } = useCharacterDetailsQuery(detailsId);
 
-  const [character, setCharacter] = useState<Character | null>(null);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState<string | null>(null);
-
-  useEffect(() => {
-    if (!detailsId) return;
-
-    setLoading(true);
-    setError(null);
-
-    fetch(`https://rickandmortyapi.com/api/character/${detailsId}`)
-      .then((res) => {
-        if (!res.ok) throw new Error('Failed to load character');
-        return res.json();
-      })
-      .then((data) => setCharacter(data))
-      .catch((err) => {
-        setError(err.message);
-        navigate('/not-found');
-      })
-      .finally(() => setLoading(false));
-  }, [detailsId, navigate]);
-
-  if (loading) return <div className="p-4">In progress...</div>;
-
-  if (error) return <div className="p-4 text-red-500">Error: {error}</div>;
-
+  if (!detailsId) return null;
+  if (isLoading) return <div className="p-4">In progress...</div>;
+  if (error)
+    return (
+      <div className="p-4 text-red-500">Error: {(error as Error).message}</div>
+    );
   if (!character) return null;
 
   return (
