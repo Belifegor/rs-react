945a858b045a86db5e2e3b405891dd2f8400eb14
github-usernamebelifegor <hydrox27@gmail.com>
2025-08-17 23:08:47 +0300

    feat: add internationalization with next-intl

---
+++ src/app/[locale]/layout.tsx
@@ -0,0 +1,24 @@
+import { NextIntlClientProvider, useMessages } from 'next-intl';
+import Providers from './providers';
+import './../globals.css';
+import './../index.css';
+
+export default function RootLayout({
+  children,
+  params: { locale },
+}: {
+  children: React.ReactNode;
+  params: { locale: string };
+}) {
+  const messages = useMessages();
+
+  return (
+    <html lang={locale}>
+      <body>
+        <NextIntlClientProvider locale={locale} messages={messages}>
+          <Providers>{children}</Providers>
+        </NextIntlClientProvider>
+      </body>
+    </html>
+  );
+}
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import Link from 'next/link';
+import { Link } from '../i18n/navigation';
 import Image from 'next/image';
 import type { Character } from '../types/types';
 import { useStore } from '../store/store';
